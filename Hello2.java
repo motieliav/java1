912d8ab2a68c1c0a36da4f212470dd43e1095d67
Moti Eliav <moti.eliav@ValueAge.com>
2015-03-15 16:10:36 +0200

    Second class invocation

--- Hello2.java
+++ Hello2.java
@@ -1,6 +1,8 @@
 
 public class Hello2 {
-	public static void main(String[] args) {
-		System.out.println("Hello JAVA World 2");
+	
+	public void helloAgain(String str) {
+		System.out.println("Hello 2 " + str);
 	}
+
 }
