71263c36453bf9fd9e6414fbe04576d660c804a5
Moti Eliav <moti.eliav@ValueAge.com>
2015-03-24 15:15:05 +0200

    Primes v1

---
+++ Prime_simple/Prime.java
@@ -0,0 +1,10 @@
+package Prime_simple;
+
+public class Prime {
+	
+	public void compute(int max) {
+		for (int i=0; i<max; i++)
+			System.out.print(i + " ");
+	}
+
+}
---
+++ Prime_simple/prime1.java
@@ -0,0 +1,12 @@
+package Prime_simple;
+
+public class prime1 {
+	public static void main(String args[]) {
+		System.out.println("Starting computing......");
+		
+		Prime p = new Prime();
+		p.compute(10);
+		
+		System.out.println("\nDONE computing.");
+	}
+}
