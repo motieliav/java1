ff6208eb737397fc5269196f747e8595744d2df9
Moti Eliav <moti.eliav@ValueAge.com>
2015-03-31 23:12:50 +0300

    CLI testing#

--- Prime_simple/prime1.java
+++ Prime_simple/prime1.java
@@ -1,5 +1,7 @@
 package Prime_simple;
 
+import Prime_simple.PrimeCalc;
+
 public class prime1 {
 	public static void main(String args[]) {
 		System.out.println("Starting computing......");
@@ -8,6 +10,6 @@ public class prime1 {
 		p.compute();
 		p.print_results();
 		
-		System.out.println("\nDONE computing.");
+		System.out.println("\nDONE prime computing.");
 	}
 }
